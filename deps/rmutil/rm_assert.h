1525a5a322eca6a7770932eae36534ecb2c5fc03
Christian <xtianus@live.com>
2024-04-08 00:57:14 -0400

    updated rm assert

--- deps/rmutil/rm_assert.h
+++ deps/rmutil/rm_assert.h
@@ -1,9 +1,8 @@
-/*
+/* 
  * Copyright Redis Ltd. 2016 - present
  * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
  * the Server Side Public License v1 (SSPLv1).
  */
-
 #ifndef __REDISEARCH_ASSERT__
 #define __REDISEARCH_ASSERT__
 
@@ -11,25 +10,25 @@
 #include "module.h"
 
 #ifdef NDEBUG
-
-#define RS_LOG_ASSERT(ctx, condition, fmt, ...)    (__ASSERT_VOID_CAST (0))
-#define RS_LOG_ASSERT_STR(ctx, condition, str)     (__ASSERT_VOID_CAST (0))
-
+#define RS_LOG_ASSERT(ctx, condition, fmt, ...) ((void)0)
+#define RS_LOG_ASSERT_STR(ctx, condition, str) ((void)0)
 #else
+#include <assert.h>
+#define RS_LOG_ASSERT(ctx, condition, fmt, ...) \
+    do { \
+        if (__builtin_expect(!(condition), 0)) { \
+            RedisModule_Log(ctx, "warning", (fmt), __VA_ARGS__); \
+            assert(condition); /* Crashes server and create a crash report */ \
+        } \
+    } while (0)
+#define RS_LOG_ASSERT_STR(ctx, condition, str) RS_LOG_ASSERT(ctx, condition, "%s", str)
+#endif // NDEBUG
 
-#define RS_LOG_ASSERT_FMT(condition, fmt, ...)                                          \
-    if (__builtin_expect(!(condition), 0)) {                                            \
-        RedisModule_Log(RSDummyContext, "warning", (fmt), __VA_ARGS__);                 \
-        RedisModule_Assert(condition); /* Crashes server and create a crash report*/    \
-    } 
+#define RS_CHECK_FUNC(funcName, ...) \
+    do { \
+        if (funcName) { \
+            funcName(__VA_ARGS__); \
+        } \
+    } while (0)
 
-#define RS_LOG_ASSERT(condition, str)  RS_LOG_ASSERT_FMT(condition, str "%s", "")
-
-#endif  //NDEBUG
-
-#define RS_CHECK_FUNC(funcName, ...)                                          \
-    if (funcName) {                                                           \
-        funcName(__VA_ARGS__);                                                \
-    } 
-
-#endif  //__REDISEARCH_ASSERT__
+#endif // __REDISEARCH_ASSERT__
